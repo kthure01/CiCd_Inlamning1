610061b19c49761774590ea1cfc224602bfebfd8
kthure01 <kent.ho.thureson@gmail.com>
2021-05-04 15:57:46 +0200

    Modified Report.md

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,8 @@
+# Importerar funktionerna för kalkylatorn
 import calculator
 
 
+# Denna testmetod kontrollera att additionsmetoden fungerar som den ska.
 def test_add_method():
     assert 1 == calculator.addition(0, 1)
     assert 1 == calculator.addition(1, 0)
@@ -9,13 +11,11 @@ def test_add_method():
     assert 2 == calculator.addition(1, 1)
     assert 3 == calculator.addition(1, 2)
     assert 3 == calculator.addition(2, 1)
-<<<<<<< HEAD
     assert 3 == calculator.addition(1, 2)
-=======
     assert 3 == calculator.addition(2, 1)
->>>>>>> 493be072020cd11614658587b514287773de38d3
 
 
+# Denna metod kontrollerar att subtraktionsmetoden fungerar som den ska.
 def test_subtract_method():
     assert -1 == calculator.subtract(0, 1)
     assert -2 == calculator.subtract(0, 2)
